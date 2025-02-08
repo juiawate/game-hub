b885b6a2885d8a68a1dee10c97c87d1e0ba63fae
juiawate <jui.awate@gmail.com>
2025-02-08 09:51:47 -0800

    created dark mode and nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <Button variant="solid">Click me</Button>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
