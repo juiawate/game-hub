841007a5efd95e31783346717f33bad60e89a294
juiawate <jui.awate@gmail.com>
2025-03-19 22:39:37 -0700

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,11 +8,13 @@ const apiClient = new APIClient<Game>('/games');
 
 export interface Game {
     id: number;
+    slug: string;
     name: string;
     background_image: string;
     parent_platforms: { platform: Platform }[];
     metacritic: number;
     rating_top: number;
+    description_raw: string;
 }
 
 const useGames = () => {
